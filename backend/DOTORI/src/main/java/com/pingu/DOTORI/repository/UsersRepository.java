21a5696a17f4f0ab0dd78f55f19730559025acca
GCH <gch6894@naver.com>
2025-08-27 16:46:50 +0900

    JM,SY_merge

--- backend/DOTORI/src/main/java/com/pingu/DOTORI/repository/UsersRepository.java
+++ backend/DOTORI/src/main/java/com/pingu/DOTORI/repository/UsersRepository.java
@@ -1,8 +1,13 @@
 package com.pingu.DOTORI.repository;
 
+import com.pingu.DOTORI.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
-public class UsersRepository {
-
+public interface UsersRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findByEmail(String email);
+    Optional<Users> findByNickName(String nickName);
+    Optional<Users> findByPhone(String phone);
 }
