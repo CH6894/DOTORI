8c0ea3bc2995fcd7998ddc7cbd28dede976ed30f
JHW <ruby07025@gmail.com>
2025-09-03 20:39:58 +0900

    calendar최종3

--- backend/DOTORI/src/main/java/com/pingu/DOTORI/config/CalendarSecurityConfig.java
+++ backend/DOTORI/src/main/java/com/pingu/DOTORI/config/CalendarSecurityConfig.java
@@ -25,21 +25,4 @@ public class CalendarSecurityConfig {
     return http.build();
   }
 
-	//2) 나머지 전체 (**반드시 마지막**, any request)
-	@Bean
-	@Order(2)
-	SecurityFilterChain main(HttpSecurity http) throws Exception {
-	http
-	.securityMatcher("/**")
-	.cors(Customizer.withDefaults())
-	.csrf(csrf -> csrf.disable())
-	.authorizeHttpRequests(auth -> auth
-	.requestMatchers("/api/public/**", "/actuator/**").permitAll()
-	.anyRequest().authenticated()   // <-- 여기만 바뀜: 인증된 사용자만 접근
-	)
-	   .oauth2Login(Customizer.withDefaults()); // <-- 여러 아이디(OAuth2 로그인) 허용
-	return http.build();
-
-}
-
 }
