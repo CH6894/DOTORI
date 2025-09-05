2ed4754745bf77c2335661385d662117c1b89b0b
GCH <gch6894@naver.com>
2025-09-05 10:57:02 +0900

    HW_merge

--- backend/DOTORI/src/main/java/com/pingu/DOTORI/entity/Orders.java
+++ backend/DOTORI/src/main/java/com/pingu/DOTORI/entity/Orders.java
@@ -9,33 +9,36 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Entity(name = "Orders")
+@Entity
 @Table(name = "orders")
 public class Orders {
+   
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Column(name = "id", nullable = false)
+   private Long id;
+   
+   @ManyToOne(fetch = FetchType.LAZY)
+   @JoinColumn(name = "item_id", nullable = false, foreignKey = @ForeignKey(name = "FK_Item_details_TO_Orders"))
+   private ItemDetails itemDetails;
+   
+   
+   @Column(name = "pay_time", nullable = false)
+   private LocalDateTime payTime;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id", nullable = false)
-	private Long id;
+   @ManyToOne(fetch = FetchType.LAZY, optional = false)
+   @JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(name = "FK_Users_TO_Orders"))
+   private Users user;
+   
+   @Column(name = "depositer_name", length = 50)
+   private String depositerName;
 
-	@Column(name = "pay_method", nullable = false, length = 100)
-	private String payMethod;
+   @Column(name = "pay_method", nullable = false, length = 100)
+   private String payMethod;
 
-	@Column(name = "pay_time", nullable = false)
-	private LocalDateTime payTime;
+   @Lob
+   @Column(name = "pay_message", columnDefinition = "TEXT")
+   private String payMessage;
 
-	@Lob
-	@Column(name = "pay_message")
-	private String payMessage;
-
-	@Column(name = "depositer_name", length = 50)
-	private String depositerName;
-
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "item_id", nullable = false, foreignKey = @ForeignKey(name = "FK_Item_details_TO_Orders"))
-	private ItemDetails itemDetails;
-
-	@ManyToOne(fetch = FetchType.LAZY, optional = false)
-	@JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(name = "FK_Users_TO_Orders"))
-	private Users user;
+   
 }
